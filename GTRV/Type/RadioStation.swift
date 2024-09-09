e91f373cc7e77e27ed3dbe1d7bdd01fdafc9c506
grandtheftradio <141779822+grandtheftradio@users.noreply.github.com>
2024-09-09 17:28:49 -0500

    Update RadioStation.swift
    progress 09092024

--- GTRV/Type/RadioStation.swift
+++ GTRV/Type/RadioStation.swift
@@ -16,6 +16,7 @@ public struct RadioStation: Identifiable {
 	let root: String?
 	let songs: [Song]?
 	let general: [String]?
+    let general_root: String?
 	let sid: [String]?
 	let mono_solo: [String]?
 	let time: [String: [String]]?
@@ -24,3 +25,24 @@ public struct RadioStation: Identifiable {
 		number
 	}
 }
+/*
+public struct RadioStation: Identifiable {
+	let index: Int
+	let number: Int
+	let name: String
+	let image: String
+	let imagePosition: Position
+	let order: String
+	let root: String?
+	let songs: [Song]?
+	let general: [String]?
+	let general_root: String?
+	let sid: [String]?
+	let mono_solo: [String]?
+	let time: [String: [String]]?
+	let to: [String: [String]]?
+	public var id: Int {
+		number
+	}
+}
+*/
